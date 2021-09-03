cb99931ede3393bedbf6b1cbb6f16f1dcf6f806a
githashem <PersonalHashem@gmail.com>
2021-09-03 19:06:45 +0430

    chore: ignore 'media' and 'staticfiles' folders

--- Eshop/settings.py
+++ Eshop/settings.py
@@ -127,13 +127,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'cdn', 'static')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'cdn', 'media')
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
